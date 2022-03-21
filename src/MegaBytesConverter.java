785540fe250ccbb4aff475d027b5637818637eb4
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-03-21 15:05:56 +0300

    Huseyin Kartal has done HW004.

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -1,3 +1,21 @@
 public class MegaBytesConverter {
-    //TODO: Please open the readme.txt file and answer 3rd question.
+    public static void main(String[] args) {
+
+        int[] inputs = {2500,-1024,5000};
+
+        for (int input:inputs) {
+            printMegaBytesAndKiloBytes(input);
+        }
+    }
+
+    private static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        int megaBytes=kiloBytes/1024;
+        int remainingKiloBytes=kiloBytes%1024;
+
+        if(kiloBytes<0){
+            System.out.println("\"Invalid Value\" because parameter is less than 0.");
+        }else{
+            System.out.println(kiloBytes+" KB = "+megaBytes+" MB and "+remainingKiloBytes+" KB");
+        }
+    }
 }
