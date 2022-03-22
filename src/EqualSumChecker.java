61d2758551ddd48d5cf6d3873494871441334ce3
Yasin Ekin <ekinyasin90@gmail.com>
2022-03-22 14:57:26 +0300

    All homeworks done

--- src/EqualSumChecker.java
+++ src/EqualSumChecker.java
@@ -1,3 +1,14 @@
 public class EqualSumChecker {
-    //TODO: Please open the readme.txt file and answer 5th question.
+
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(2,2,4));
+        System.out.println(hasEqualSum(1,2,5));
+    }
+    public static boolean hasEqualSum (int a,int b,int c){
+            if((a+b)==c){
+                return true;
+            }else{
+                return false;
+            }
+    }
 }
