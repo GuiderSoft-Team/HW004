92a44631fcddf57c0aeb9bd37f888aa02daa47fa
90551 <akarsu64engin@gmail.com>
2022-03-22 23:53:35 +0300

    Eq

--- src/EqualSumChecker.java
+++ src/EqualSumChecker.java
@@ -1,3 +1,20 @@
 public class EqualSumChecker {
-    //TODO: Please open the readme.txt file and answer 5th question.
+    //Please open the readme.txt file and answer 5th question.
+
+    public static void main(String[] args) {
+        System.out.println((hasEqualSum(5,6,11)));
+        System.out.println(hasEqualSum(7,7,6));
+        System.out.println();
+        System.out.println("HAYIRLI ÇALIŞMALAR");
+    }
+
+    public static boolean hasEqualSum(int x,int y,int z){
+            if(x+y==z){
+                return true;
+            }else{
+                return false;
+            }
+    }
+
+
 }
