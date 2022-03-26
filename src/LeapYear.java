e66aa8692c5dfa3a81fa0d454b4f37d3af7d2215
AlkanTuncer <alkantuncer53@hotmail.com>
2022-03-26 13:22:00 +0300

    Alkan Tuncer HW004 DONE.

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,4 +1,35 @@
 public class LeapYear {
-    //TODO: Please open the readme.txt file and answer 4th question.
+
+    public static boolean isLeapYear(int year) {
+
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println("The year is a leap year (it has 366 days).");
+                        return true;
+                    }
+                } else {
+                    System.out.println("The year is not a leap year (it has 365 days).");
+                    return false;
+                }
+            } else {
+                System.out.println("The year is not a leap year (it has 365 days).");
+                return false;
+            }
+            return true;
+        } else {
+            System.out.println("the parameter is not in range (1-9999)");
+            return false;
+        }
+    }
+
+
+        public static void main (String[]args){
+            System.out.println(isLeapYear(-1600));
+            System.out.println(isLeapYear(1600));
+            System.out.println(isLeapYear(2017));
+            System.out.println(isLeapYear(2000));
+        }
 
 }
--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,3 +1,27 @@
 public class SpeedConverter {
-    //TODO: Please open the readme.txt file and answer 1st and 2nd questions.
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609);
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if (kilometersPerHour<0){
+            System.out.println("INVALID VALUE");
+        }else{
+            System.out.println(kilometersPerHour+ "km/h ="+toMilesPerHour(kilometersPerHour)+" mi/h");
+        }
+    }
+
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+
+    }
 }
