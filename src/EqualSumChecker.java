96c336faf7abc5029d0df517008c5d407bb3d26d
gazimurat <yemliha.cooker@gmail.com>
2022-04-05 23:38:29 +0300

    HW4

--- src/EqualSumChecker.java
+++ src/EqualSumChecker.java
@@ -1,3 +1,21 @@
 public class EqualSumChecker {
-    //TODO: Please open the readme.txt file and answer 5th question.
-}
+    public static void main(String[] args) {
+
+        System.out.println(hasEqualSum(1,1,1));
+        System.out.println(hasEqualSum(1,1,2));
+        System.out.println(hasEqualSum(1,-1,0));
+
+    }
+
+    public static boolean hasEqualSum(int x, int y, int z) {
+        if(x + y == z){
+            return true;
+
+        } else {
+            return false;
+        }
+
+
+    }
+
+    }
--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -1,3 +1,31 @@
 public class MegaBytesConverter {
-    //TODO: Please open the readme.txt file and answer 3rd question.
+    public static void main(String[] args) {
+
+printMegaBytesAndKiloBytes(2500);
+printMegaBytesAndKiloBytes(-1024);
+printMegaBytesAndKiloBytes(5000);
+
+
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+           int megaByte = (kiloBytes/1024);
+           int remainingByte= (kiloBytes%1024);
+
+           String result=kiloBytes + " KB = " + megaByte + " MB and " + remainingByte + " MB ";
+
+                   System.out.println (result);
+
+
+
+
+
+
+
+
+        }
+    }
 }
