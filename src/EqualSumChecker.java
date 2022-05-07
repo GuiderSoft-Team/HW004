ed630bd8bde6a1d49dc7bd4fc4e31fd7090432bc
EnesKula <100785101+EnesKula@users.noreply.github.com>
2022-05-07 16:51:57 +0300

    Enex HW004

--- src/EqualSumChecker.java
+++ src/EqualSumChecker.java
@@ -1,3 +1,14 @@
 public class EqualSumChecker {
-    //TODO: Please open the readme.txt file and answer 5th question.
+
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1,2,3));
+        System.out.println(hasEqualSum(5,4,3));
+        System.out.println(hasEqualSum(1,7,9));
+        System.out.println(hasEqualSum(7,-2,5));
+    }
+
+    public static boolean hasEqualSum(int x,int y, int z){
+        return x + y == z;
+    }
+
 }
--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -1,3 +1,17 @@
 public class MegaBytesConverter {
-    //TODO: Please open the readme.txt file and answer 3rd question.
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2090);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        int kb=kiloBytes%1024;
+        int mb=kiloBytes/1024;
+        if (kiloBytes<0){
+            System.out.println("Invalid Value");
+        }else {
+            System.out.println(kiloBytes+"KB = "+mb+" MB and "+kb+" KB");
+        }
+    }
+
 }
