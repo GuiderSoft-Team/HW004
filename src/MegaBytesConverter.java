89793a2c93da47b500ee3bf54e1d4f4d132d69ca
mthntl <mthanktl@gmail.com>
2022-05-10 12:10:53 +0300

    HW004 has been created."

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -1,3 +1,16 @@
 public class MegaBytesConverter {
-    //TODO: Please open the readme.txt file and answer 3rd question.
+    public static void main(String[] args) {
+        printMbAndKb(2000);
+    }
+    public static void printMbAndKb(int kb){
+
+        kb = kb % 1024;
+        int mb=kb/1024;
+
+        if(kb<0){
+            System.out.println("GEÇERSİZ DEĞER");
+        }else {
+            System.out.println(kb+"KB = "+mb+" MB ve "+kb+" KB");
+        }
+    }
 }
