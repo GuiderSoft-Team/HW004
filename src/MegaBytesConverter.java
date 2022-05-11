cf2588eb2dee77ef9f9288ecde78629a302b968d
stokatlio2012 <stokatlio2012@gmail.com>
2022-05-11 22:22:05 +0300

    Project done.

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -1,3 +1,17 @@
 public class MegaBytesConverter {
-    //TODO: Please open the readme.txt file and answer 3rd question.
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(2048);
+        printMegaBytesAndKiloBytes(-4563);
+    }
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        int megaBytes = kiloBytes / 1024;
+        int overKiloBytes = (kiloBytes % 1024);
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value!");
+        } else {
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB" + (overKiloBytes != 0 ? " and " + overKiloBytes + " KB":""));
+        }
+
+    }
 }
