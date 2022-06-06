5e24e117b5633a4f1e26cb38468cbb8902f81529
NurannNalci <nuran.nalann2411@gmail.com>
2022-06-06 18:07:33 +0200

    HW004

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,4 +1,22 @@
 public class LeapYear {
     //TODO: Please open the readme.txt file and answer 4th question.
-
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1700));
+        System.out.println(isLeapYear(1600));
+    }
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if(year%4==0){
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("This year is a leap year and it has 366 days");
+                    return true;
+                } else {
+                    System.out.println("This year is not a leap year.Since it has 365 days");
+                }
+            }else return false;
+        }else return false;
+    }
+          return false;
+      }
 }
--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,3 +1,19 @@
 public class SpeedConverter {
-    //TODO: Please open the readme.txt file and answer 1st and 2nd questions.
+    public static void main(String[] args) {
+    }
+    public static long toMilesPerHour (double kilometersPerHour) {
+        long roundedValue = Math.round(kilometersPerHour/1.609);
+        if(roundedValue>=0) {
+            return roundedValue;
+        }else{
+            return -1;
+        }
+    }
+    public static void PrintConversion (double kilometersPerHour) {
+        if(kilometersPerHour<0){
+            System.out.println("INVALID VALUE");
+        }else{
+            System.out.println(kilometersPerHour+"km/h = "+toMilesPerHour(kilometersPerHour)+"mi/h");
+        }
+    }
 }
